23e48aff3dbb0f71637dafc37c4da1b87c371133
Sarah D Herren <herren.sarah@icloud.com>
2021-06-02 15:42:19 -0500

    mvp

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function MovieList(props) {
+
+
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link to={`/movies/${movie.id}`} key={movie.id}>
+          <MovieDetails 
+            movie={movie} 
+          /> 
+        </Link>
       ))}
     </div>
   );
